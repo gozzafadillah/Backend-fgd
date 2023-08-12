0b9f83712be2164a0b76a3ec625389356579d328
Muhammad Fadillah Abdul Aziz <74459029+gozzafadillah@users.noreply.github.com>
2023-08-13 02:02:16 +0700

    fix error

--- driver/mongo/mongo.go
+++ driver/mongo/mongo.go
@@ -2,6 +2,7 @@ package mongo_driver
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -17,6 +18,7 @@ func Init(databaseName string) *mongo.Database {
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
 	dbHost := os.Getenv("DB_HOST")
+	fmt.println(dbUser, dbPass, dbHost)
 	mongodbURI := "mongodb://mongo:U73R5S6hkwjlEwAy4e3H@containers-us-west-45.railway.app:5758"
 
 	// Set client options
